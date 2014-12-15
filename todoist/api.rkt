f598c9405432356acf3fc9319467c0b19ce64391
Jonas Rodrigues <jxonas@gmail.com>
2014-12-15 14:57:48 -0200

    user operations

--- todoist/api.rkt
+++ todoist/api.rkt
@@ -1,33 +1,120 @@
 #lang racket/base
 
-(require json
+(require (for-syntax racket/base
+                     racket/syntax
+                     syntax/parse)
+         json
          net/http-client
          net/uri-codec
          racket/format
-         racket/port
-         "config.rkt")
+         racket/match
+         racket/port)
 
-(define API-VERSION "5.3")
+(struct response (status-line header body))
 
-(define .todoist (build-path (find-system-path 'home-dir) ".todoist"))
-(define token (make-parameter (get-config 'token "..." .todoist)))
+(define (parse-status-line str)
+  (match (~a str)
+    [(pregexp "^HTTP/(.*)\\s(\\d+)\\s(\\S*)$" (list _ version code message))
+     (values version (string->number code) message)]))
 
-(define (request [data-alist '()] #:method [method "get"] #:seq [seq 0])
-  (define-values (return headers body)
-    (http-sendrecv "api.todoist.com"
-                   (format "/TodoistSync/v~a/~a" API-VERSION method)
-                   #:method "POST"
-                   #:ssl? #t
-                   #:headers '("Content-Type: application/x-www-form-urlencoded")
-                   #:data
-                   (alist->form-urlencoded
-                    (list* (cons 'api_token (~a (token)))
-                           (cons 'seq_no (~a seq))
-                           (normalize-data data-alist)))))
-  (if (bytes=? return #"HTTP/1.1 200 OK")
-      (string->jsexpr (port->string body))
-      (error (~a return))))
+(define (->jsexpr x)
+  (match x
+    [(response status-line header body) (string->jsexpr body)]
+    [else (string->jsexpr (~a x))]))
 
-(define (normalize-data data-alist)
-  (for/list ([data (in-list data-alist)])
-    (cons (car data) (~a (cdr data)))))
+(define (response-code r)
+  (define-values (version code message)
+    (parse-status-line (response-status-line r)))
+  code)
+
+(define (response-message r)
+  (define-values (version code message)
+    (parse-status-line (response-status-line r)))
+  message)
+
+
+(define (request op [data '()]
+                 #:method [method "POST"]
+                 #:host [host "todoist.com"]
+                 #:headers [headers '("Content-Type: application/x-www-form-urlencoded")]
+                 #:endpoint-fmt [endpoint-fmt "/API/~a"]
+                 #:ssl? [ssl? #t])
+  (define-values (status-line header body)
+    (http-sendrecv host
+                   (format endpoint-fmt op)
+                   #:method method
+                   #:ssl? ssl?
+                   #:headers headers
+                   #:data (alist->form-urlencoded data)))
+  (response status-line header (port->string body)))
+
+
+
+(define-syntax (define-api/get stx)
+
+  (define (racket->js str)
+    (regexp-replaces str '([#rx"-" "_"] [#rx"[!?*]" ""])))
+
+  (define-syntax-class operation
+    (pattern racket:id
+             #:with js (datum->syntax #'racket (symbol->string (syntax-e #'racket))))
+    (pattern [racket:id js:str]))
+
+  (define-syntax-class argument
+    (pattern racket:id
+             #:with js (datum->syntax #'racket
+                                      (string->symbol
+                                       (racket->js
+                                        (symbol->string (syntax-e #'racket)))))
+             #:with keyword (datum->syntax #'racket
+                                           (string->keyword
+                                            (symbol->string (syntax-e #'racket))))))
+
+  (syntax-parse stx
+    #:datum-literals (:)
+    [(_ name:operation arg:argument ... : opt:argument ...)
+     (with-syntax*
+         ([default (gensym)]
+          [optionals
+           (for/fold ([l '()])
+                     ([k (in-list (syntax-e #'(opt.keyword ...)))]
+                      [r (in-list (syntax-e #'(opt.racket ...)))])
+             (list* k (list r #''default) l))])
+
+       #`(define (name.racket arg.racket ... #,@#'optionals)
+           (define data (list (cons 'arg.js arg.racket) ...))
+           (unless (eq? opt.racket 'default)
+             (set! data (cons (cons 'opt.js opt.racket) data)))
+           ...
+           (request name.js data #:method "GET")))]
+    [(_ name:operation arg:argument ...)
+     #'(define-api/get name arg ... :)]))
+
+
+;; Users
+
+(define-api/get login email password)
+
+(define-api/get (login-with-google "loginWithGoogle") email oauth2-token :
+  auto-signup full-name timezone lang)
+
+(define-api/get ping token)
+(define-api/get (get-timezones "getTimezones"))
+
+(define-api/get register email full-name password :
+  lang timezone)
+
+(define-api/get (delete-user "deleteUser") token current-password :
+  reason-for-delete in-background)
+
+(define-api/get (update-user "updateUser") token :
+  email full-name password timezone date-format time-format
+  start-day next-week start-page default-remainder)
+
+(define-api/get (update-avatar "updateAvatar") token :
+  image delete)
+
+(define-api/get (get-redirect-link "getRedirectLink") token :
+  path hash)
+
+(define-api/get (get-productivity-stats "getProductivityStats") token)
